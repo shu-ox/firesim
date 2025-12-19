7c4c4c3002291f8601be729ec077aa0a0b88d51d
steve hu <steve.hu@oxmiq.ai>
2025-12-18 17:26:08 -0800

    correct to uname -s

--- sim/make/library.mk
+++ sim/make/library.mk
@@ -7,7 +7,7 @@ midas_h  = $(shell find $(simif_dir) -name "*.h")
 midas_cc = $(shell find $(simif_dir) -name "*.cc")
 
 common_cxx_flags := $(TARGET_CXX_FLAGS) -Wno-unused-variable -DFIRESIM
-ifdef ($(shell uname -r),Linux)
+ifdef ($(shell uname -s),Linux)
 common_ld_flags := $(TARGET_LD_FLAGS) -lrt
 else
 common_ld_flags := $(TARGET_LD_FLAGS)
